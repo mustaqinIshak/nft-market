e8d4dcd5f2d3711b6217edde4fb955b09dbd5f6d
mustaqinIshak <you@example.com>
2025-04-18 16:06:04 +0800

    move custom color from postcss to globalcss

--- app/(dashboard)/component/navbar.js
+++ app/(dashboard)/component/navbar.js
@@ -1,10 +1,22 @@
 'use client';
-import { useEffect, useState, React } from 'react';
+import { useEffect, useState, useContext,React } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import images from '@/assets'
 
 const Navbar = () => {
-
+  const {theme, setTheme} = useTheme();
   return (
-    <div>Navbar</div>
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black border-nft-gray-1">
+      <Link href="/">
+        <div>
+          <Image src={images.logo02} objectFit='contain' width={32} height={32} alt='logoNavbar' />
+        </div>
+      </Link>
+    </nav>
   )
 }
 
